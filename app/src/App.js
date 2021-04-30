4917ec4de4c35e85614e728289550bbaac3bb450
Mandy <34908590+picaq@users.noreply.github.com>
2021-04-30 15:07:30 -0700

    start to edit Tree

--- app/src/App.js
+++ app/src/App.js
@@ -23,6 +23,53 @@ const App = () => {
   d3.selectAll("li")
     .append("appppppp")
     .style("color", (d, i) => rainbow[i % 4]);
+
+  const nodes = [
+ {key: 0, name: 'Birthday Party'},
+  
+ {key: 1, name: 'activities'},
+ {key: 2, name: 'party favors'},
+ {key: 3, name: 'cake'},
+ {key: 4, name: 'catering'},
+ {key: 5, name: 'venue'},
+ {key: 6, name: 'decorations'},
+  
+ {key: 7, name: 'board games'},
+ {key: 8, name: 'video games'},
+  
+ {key: 9, name: 'go to party store'},
+  
+ {key: 10, name: 'RSVPs'},
+  
+ {key: 11, name: 'balloons'},
+ {key: 12, name: 'go to bakery'},
+ {key: 13, name: 'send invitations'},
+ {key: 14, name: 'make invitations'}
+]
+
+const links = [
+  {source: 0, target: 0},
+  {source: 1, target: 0},
+  {source: 2, target: 0},
+  {source: 3, target: 0},
+  {source: 4, target: 0},
+  {source: 5, target: 0},
+  {source: 6, target: 0},
+  {source: 7, target: 1},
+  {source: 8, target: 1},
+  {source: 9, target: 2},
+  {source: 10, target: 2},
+  {source: 10, target: 3},
+  {source: 10, target: 4},
+  {source: 10, target: 5},
+  {source: 11, target: 6},
+  {source: 9, target: 11},
+  {source: 12, target: 3},
+  {source: 12, target: 3},
+  {source: 13, target: 10},
+  {source: 14, target: 13}
+];
+
   const data = [
     { year: 1980, efficiency: 24.3, sales: 8949000 },
     { year: 1985, efficiency: 27.6, sales: 10979000 },
