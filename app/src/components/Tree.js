a1f78ad3ca1967fed3238e5f9ba3abc8835a4cf9
Mandy <34908590+picaq@users.noreply.github.com>
2021-05-01 03:01:06 -0700

    add codepen d3 to Tree.js ; remove BarChart code ; add ref to svg, change style and className to match react orthography

--- app/src/components/Tree.js
+++ app/src/components/Tree.js
@@ -2,42 +2,80 @@ import { useD3 } from '../hooks/useD3';
 import React from 'react';
 import * as d3 from 'd3';
 
-function Tree({ data }) {
+// passing in
+function Tree({ links, nodes }) { // pink curlies = don’t worry about order of passing in
     const ref = useD3(
       (svg) => {
         const height = 500;
         const width = 500;
-        const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-  
-        const x = d3
-          .scaleBand()
-          .domain(data.map((d) => d.year))
-          .rangeRound([margin.left, width - margin.right])
-          .padding(0.1);
-  
- 
-        svg.select(".x-axis").call(xAxis);
-        svg.select(".y-axis").call(y1Axis);
-  
-        svg
-          .select(".plot-area")
-          .attr("fill", "steelblue")
-          .selectAll(".bar")
-          .data(data)
-          .join("rect")
-          .attr("class", "bar")
-          .attr("x", (d) => x(d.year))
-          .attr("width", x.bandwidth())
-          .attr("y", (d) => y1(d.sales))
-          .attr("height", (d) => y1(0) - y1(d.sales));
+        // const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+
+        d3.forceSimulation(nodes)
+          .force('charge', d3.forceManyBody().strength(-400))
+          .force('center', d3.forceCenter(width, height))
+          .force('link', d3.forceLink().links(links))
+          .on('tick', ticked);
+        // console.log("line 18", simulation);
+        function updateLinks() {
+          const u = d3.select('.links')
+            .selectAll('line')
+            .data(links)
+            u.enter()
+            .append('line')
+            .merge(u)
+            .attr('x1', function(d) {
+              return d.source.x
+            })
+            .attr('y1', function(d) {
+              return d.source.y
+            })
+            .attr('x2', function(d) {
+              return d.target.x
+            })
+            .attr('y2', function(d) {
+              return d.target.y
+            })
+        
+          u.exit().remove()
+        }
+        
+        function updateNodes() {
+          const u = d3.select('.nodes')
+            .selectAll('text')
+            .data(nodes)
+        
+          u.enter()
+            .append('text')
+            .text(function(d) {
+              return d.name
+            })
+            .merge(u)
+            .attr('x', function(d) {
+              return d.x
+            })
+            .attr('y', function(d) {
+              return d.y
+            })
+            .attr('dy', function(d) {
+              return 5
+            })
+        
+          u.exit().remove()
+        }
+        
+        function ticked() {
+          updateLinks()
+          updateNodes()
+        }
+        
       }
     );
   
     return (
 
       <svg
-        ref={ref}
-       style="width: 100%; height: 100%">
+       ref={ref}
+       style={{width: "100%", height: "100%"}}>
       <defs>
     <marker id="triangle" viewBox="0 0 10 10"
           refX="1" refY="5"
@@ -48,8 +86,8 @@ function Tree({ data }) {
     </marker>
   </defs>
       
-      <g class="links"></g>
-      <g class="nodes"></g>
+      <g className="links"></g>
+      <g className="nodes"></g>
     </svg>
 
     );
