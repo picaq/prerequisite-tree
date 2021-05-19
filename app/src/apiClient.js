6b8814f23f0c4111a9ab7c8323f6d1d689364a9b
George Song <george@monozuku.com>
2021-05-19 11:24:16 -0700

    fix: remove dotenv sourced envvars since it's not valid within CRA

--- app/src/apiClient.js
+++ app/src/apiClient.js
@@ -1,10 +1,3 @@
-import dotenv from "dotenv";
-
-// export const getTasks = async () => {
-//   const response = await fetch("/api/tasks");
-//   return response.json();
-// };
-
 export const addTask = async (saveData) => {
   const response = await fetch("/api/tasks", {
     method: "POST",
@@ -33,15 +26,10 @@ export const getGraph = async () => {
 };
 
 export const getImage = async () => {
-  // dotenv.config({ path: "../../.env" });
-  dotenv.config({ path: "../.env" });
-
   const response = await fetch(
     `https://api.nasa.gov/planetary/apod?api_key=${
       process.env.REACT_APP_NASA_API_KEY
         ? process.env.REACT_APP_NASA_API_KEY
-        : process.env.NASA_API_KEY
-        ? process.env.NASA_API_KEY
         : "DEMO_KEY"
     }`,
     {
