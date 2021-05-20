caf348a5d3ab024a00b9fa6ce1b200046d97258a
George Song <george@monozuku.com>
2021-05-20 16:28:08 -0700

    fix: don't try to render image unless there is one to render

--- app/src/components/Tree.js
+++ app/src/components/Tree.js
@@ -82,23 +82,25 @@ function Tree({ links, nodes, image }) {
         // style={{width: "100%", height: "100%"}}
         style={{ overflow: "visible" }}
       >
-        <image
-          style={{
-            backgroundImage: `url(${image.hdurl})`,
-            // backgroundColor: "pink",
-            backgroundSize: "cover",
-            opacity: ".32",
-            overflow: "visible",
-            enableBackground: "new",
-            transform: `translateY(calc(-66vh + 66vmin)) translateX(calc(-55vw + 55vmin))`,
-            position: "absolute",
-            top: 0,
-          }}
-          // width={window.innerWidth}
-          // width="100%"
-          // height={window.innerHeight}
-          href={image.hdurl}
-        ></image>
+        {image?.hdurl ? (
+          <image
+            style={{
+              backgroundImage: `url(${image.hdurl})`,
+              // backgroundColor: "pink",
+              backgroundSize: "cover",
+              opacity: ".32",
+              overflow: "visible",
+              enableBackground: "new",
+              transform: `translateY(calc(-66vh + 66vmin)) translateX(calc(-55vw + 55vmin))`,
+              position: "absolute",
+              top: 0,
+            }}
+            // width={window.innerWidth}
+            // width="100%"
+            // height={window.innerHeight}
+            href={image.hdurl}
+          ></image>
+        ) : null}
         <defs>
           <marker
             id="triangle"
